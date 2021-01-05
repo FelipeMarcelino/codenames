77ae1de543b61048f09ab9a2aa8b8f3d3a61b774
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2021-01-05 20:53:40 -0300

    First commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A codenames game with AI player using reinforcement learning. ',
+    author='Felipe Marcelino & Roberta Viola ',
+    license='',
+)
